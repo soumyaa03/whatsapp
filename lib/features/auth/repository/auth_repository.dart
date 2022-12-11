3e1b6254595b3ffd5c72bc26619b5d9f6b67a417
soumyaa03 <82571869+soumyaa03@users.noreply.github.com>
2022-12-12 03:35:13 +0530

    fourth

---
+++ lib/features/auth/repository/auth_repository.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:whatsapp/common/utils/utils.dart';
+import 'package:whatsapp/features/auth/screens/otp_screen.dart';
+
+class AuthRepository {
+  final FirebaseAuth auth;
+  final FirebaseFirestore firestore;
+  AuthRepository({
+    required this.auth,
+    required this.firestore,
+  });
+
+  void signInWithPhone(BuildContext context, String phoneNumber) async {
+    try {
+      await auth.verifyPhoneNumber(
+          phoneNumber: phoneNumber,
+          verificationCompleted: (PhoneAuthCredential credential) async {
+            await auth.signInWithCredential(credential);
+          },
+          verificationFailed: (error) {
+            throw Exception(error.message);
+          },
+          codeSent: ((String verificationId, int? resendToken) async {
+            Navigator.pushNamed(context, OTPScreen.routeName,
+                arguments: verificationId);
+          }),
+          codeAutoRetrievalTimeout: ((String verificationId) {}));
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
+  }
+}
